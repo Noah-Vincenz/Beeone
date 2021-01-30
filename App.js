0638b109ccc5d615777a401b280ea9509aa358bc
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2021-01-30 14:46:12 +0000

    Fixed local run.

--- App.js
+++ App.js
@@ -9,18 +9,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-function App() {
+const NavStack = () => { // declare NavStack as fat arrow functions
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomeScreen">
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Home' }}/>
-        <Stack.Screen name="MyNewsFeedScreen" component={MyNewsFeedScreen} options={{ title: 'News Feed' }}/>
-        <Stack.Screen name="MyBankingScreen" component={MyBankingScreen} options={{ title: 'Banking' }}/>
-        <Stack.Screen name="MyDocumentsScreen" component={MyDocumentsScreen} options={{ title: 'Documents' }} />
-        <Stack.Screen name="MyMarketplaceScreen" component={MyMarketplaceScreen} options={{ title: 'Marketplace' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Home' }}/>
+      <Stack.Screen name="MyNewsFeedScreen" component={MyNewsFeedScreen} options={{ title: 'News Feed' }}/>
+      <Stack.Screen name="MyBankingScreen" component={MyBankingScreen} options={{ title: 'Banking' }}/>
+      <Stack.Screen name="MyDocumentsScreen" component={MyDocumentsScreen} options={{ title: 'Documents' }} />
+      <Stack.Screen name="MyMarketplaceScreen" component={MyMarketplaceScreen} options={{ title: 'Marketplace' }} />
+    </Stack.Navigator>
   );
 }
 
-export default App;
+export default App = () => {
+  return (
+    <NavigationContainer>
+      <NavStack />
+    </NavigationContainer>
+  )
+}
